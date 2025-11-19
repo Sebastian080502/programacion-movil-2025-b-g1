40a028362925404f78a6e20d4c1313669453cec8
Juan Sebastian Osorio Fierro <jsosorio-2032b@corhuila.edu.co>
2025-11-19 16:45:38 -0500

    add new functionalities

--- backend/backend_app/src/main.ts
+++ backend/backend_app/src/main.ts
@@ -6,7 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,13 +13,17 @@ async function bootstrap() {
     }),
   );
 
-
   app.enableCors({
-    origin: true,        
+    origin: [
+      'http://localhost:8100',     // Ionic en navegador
+      'http://localhost',          // por si algo usa localhost plano
+      'capacitor://localhost',     // cuando lo abras en APK
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
 
-  
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
 }
