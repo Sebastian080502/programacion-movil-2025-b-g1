183b750c8f98166ebb66e5742a24d4cdc4839eb5
Juan Sebastian Osorio Fierro <jsosorio-2032b@corhuila.edu.co>
2025-11-19 16:35:31 -0500

    fix main.ts and enable CORS

--- backend/backend_app/src/main.ts
+++ backend/backend_app/src/main.ts
@@ -5,8 +5,24 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({ origin: true, credentials: true });
-   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+
+  app.enableCors({
+    origin: true,        
+    credentials: true,
+  });
+
+  
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
 }
+
 bootstrap();
