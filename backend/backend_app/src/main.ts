04f233be5feca4c9f2af1c9936f871756bf1079d
Juan Sebastian Osorio Fierro <jsosorio-2032b@corhuila.edu.co>
2025-11-19 18:25:45 -0500

    add new functionalities

--- backend/backend_app/src/main.ts
+++ backend/backend_app/src/main.ts
@@ -2,11 +2,12 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import type { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
+  // Validaciones globales
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,17 +15,30 @@ async function bootstrap() {
     }),
   );
 
+  // Middleware CORS manual (ignora lo de enableCors)
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    // Origen que quieres permitir (localhost Ionic)
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
 
-  app.enableCors({
-  origin: ["http://localhost:8100", "https://mobility-frontend.onrender.com"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Accept",
-    "Origin",
-  ],
-});
+    // Métodos permitidos
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+
+    // Headers permitidos
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+
+    // Responder de una vez los preflight
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+
+    next();
+  });
 
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
